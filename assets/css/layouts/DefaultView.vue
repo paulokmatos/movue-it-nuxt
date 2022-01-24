7774ab213968d96a9e57708070df813ff1e6f22d
paulokmatos <kendako361@gmail.com>
2022-01-24 18:15:03 -0300

    Adicionados componentes Profile, Experience Bar e completed Challenges

--- assets/css/layouts/DefaultView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <div class="app-bar h-1 bg-red"></div>
-    <Nuxt />
-  </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,34 @@
 <template>
-  <h1 class="">Hello World</h1>
+  <section
+    class="flex flex-col lg:flex-row flex-1 lg:flex-none lg:mt-16 sm:gap-x-10 md:gap-x-20"
+  >
+    <div class="flex flex-col w-full lg:w-1/2">
+      <Profile />
+      <CompletedChallenges />
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import CompletedChallenges from '~/components/atoms/CompletedChallenges.vue'
+import Profile from '~/components/molecules/Profile.vue'
+
+export interface Head {
+  title: string
+}
+
 export default Vue.extend({
   name: 'IndexPage',
+  head(): Head {
+    return {
+      title: 'Home | Movue-it',
+    }
+  },
+  components: {
+    CompletedChallenges,
+    Profile,
+  },
 })
 </script>
