3a46fec34d2231d90909059e3f330ab5d2827a97
paulokmatos <kendako361@gmail.com>
2022-01-24 00:25:34 -0300

    Configurada Store de componentes do Projeto

---
+++ store/Challenges/index.ts
@@ -0,0 +1,16 @@
+import { State } from './types';
+import allChallenges from "~/assets/challenges/data"
+
+
+export const state = ():State => ({
+  level: 1,
+  xp: {
+    start: 0,
+    current: 0,
+    end: 64
+  },
+  completedChallenges: 0,
+  currentChallengeIndex: null,
+  isLevelUpModalOpen: false,
+  allChallenges,
+})
