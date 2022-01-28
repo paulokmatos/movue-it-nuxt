4b00e6cbba54f677623ebebf262cd4f7d828850a
paulokmatos <kendako361@gmail.com>
2022-01-28 15:17:00 -0300

    Adicionados componentes finais, gerenciamento dos cookies e lógica da aplicação

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,42 @@
     <div class="flex flex-col w-full lg:w-1/2">
       <Profile />
       <CompletedChallenges />
+      <Countdown @completed="getNewChallenge" />
+      <button v-if="hasCountDownCompleted" disabled class="button completed">
+        Cycle completed
+      </button>
+      <button
+        v-else-if="isCountDownActive"
+        class="button abandon"
+        @click="setCountDownState(false)"
+      >
+        Abandon cycle
+      </button>
+      <button v-else class="button start" @click="setCountDownState(true)">
+        Start a cycle
+      </button>
     </div>
+    <Card id="challenge" class="w-full lg:w-1/2" />
   </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { mapState, mapGetters, mapMutations } from 'vuex'
+import { Mutations as CountdownMT } from '~/store/Countdown/types'
+import { Mutations as ChallengesMT } from '~/store/Challenges/types'
 
 import CompletedChallenges from '~/components/atoms/CompletedChallenges.vue'
 import Profile from '~/components/molecules/Profile.vue'
+import Countdown from '~/components/molecules/Countdown.vue'
+import Card from '~/components/organisms/Card.vue'
+
+import {
+  playAudio,
+  sendNotifications,
+  getRandomNumber,
+  scrollToElement,
+} from '~/utils'
 
 export interface Head {
   title: string
@@ -28,7 +55,48 @@ export default Vue.extend({
   },
   components: {
     CompletedChallenges,
+    Countdown,
     Profile,
+    Card,
+  },
+  mounted() {
+    if ('Notification' in window) {
+      Notification.requestPermission()
+    }
+  },
+  computed: {
+    ...mapState('Countdown', {
+      hasCountDownCompleted: 'hasCompleted',
+      isCountDownActive: 'isActive',
+    }),
+    ...mapGetters('Challenges', ['challengesLength']),
+  },
+  methods: {
+    ...mapMutations({
+      setCountDownHasCompleted: `Countdown/${CountdownMT.SET_HAS_COMPLETED}`,
+      setCountDownIsActive: `Countdown/${CountdownMT.SET_IS_ACTIVE}`,
+      setCurrentChallengeIndex: `Challenges/${ChallengesMT.SET_CURRENT_CHALLENGE_INDEX}`,
+    }),
+    setCountDownState(flag: boolean) {
+      this.setCountDownHasCompleted(false), this.setCountDownIsActive(flag)
+    },
+    getNewChallenge() {
+      const index = getRandomNumber(0, this.challengesLength)
+      this.setCountDownHasCompleted(true)
+      this.setCurrentChallengeIndex(index)
+
+      if (Notification?.permission === 'granted') {
+        playAudio('/notification.mp3')
+        sendNotifications('New challenge!', {
+          body: 'A new challenge has started! Go complete it!',
+          icon: '/favicon.png',
+        })
+
+        this.$nextTick(() => {
+          scrollToElement('#challenge')
+        })
+      }
+    },
   },
 })
 </script>
--- store/Countdown/index.ts
+++ store/Countdown/index.ts
@@ -1,21 +1,21 @@
-import { State, Getters, Mutations, MutationsInterface } from "./types";
+import { State, Getters, Mutations, MutationsInterface } from './types'
 
-const MINUTES = 25;
+const MINUTES = 25
 
 export const state = (): State => ({
   time: MINUTES * 60,
   isActive: false,
-  hasCompleted: false
+  hasCompleted: false,
 })
 
 export const getters: Getters = {
-  minutes: state => Math.floor(state.time / 60),
-  seconds: state => state.time % 60
+  minutes: (state) => Math.floor(state.time / 60),
+  seconds: (state) => state.time % 60,
 }
 
 export const mutations: MutationsInterface = {
   [Mutations.SET_TIME](state, newTime) {
-    state.time = newTime;
+    state.time = newTime
   },
   [Mutations.RESET_TIME](state) {
     state.time = MINUTES * 60
